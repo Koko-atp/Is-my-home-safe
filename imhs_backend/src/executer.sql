c8fa5a8e131cef76eef21521ffcf01a2dec36d62
Koko-atp <koko.atp25@gmail.com>
2026-01-23 14:17:40 +0700

    create board

--- imhs_backend/src/executer.sql
+++ imhs_backend/src/executer.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS gas_status;
-DROP TABLE IF EXISTS fire_status;
-DROP TABLE IF EXISTS temp_status;
-DROP TABLE IF EXISTS boards;
-DROP TABLE IF EXISTS rooms;
-DROP TABLE IF EXISTS homes;
-DROP TABLE IF EXISTS users;
+SELECT * FROM Room r 
+INNER JOIN Home h ON r.H_id = h.H_id
+INNER JOIN User u  ON h.U_id = u.u_id
+WHERE u.U_id = 1 AND r.r_id = 2 AND h.H_id = 1 ;
+
+
+SELECT * FROM Home h join Room r ON r.H_id = h.H_id;
+
+
+SELECT * FROM Room ;
--- imhs_backend/src/fronendapi.ts
+++ imhs_backend/src/fronendapi.ts
@@ -7,6 +7,8 @@ const createitfCreate = z.object({
     name : z.string().min(4 , "must be atleast 4 charactor"),
     email : z.email("email is in correct or missing")
 })
+
+
 const page = new Hono;
 const callDB = (c:any , exc:string)  => c.env.IMHS_DB.prepare(exc)
 
@@ -14,9 +16,8 @@ page.get('/users' , async (c) => {
     const getallsending = await callDB(c, "SELECT * FROM user").all()
 
     console.log(getallsending )
-    return c.json({"getting it" : getallsending.results})
+    return c.json({"users" : getallsending.results})
 })
-
 page.post('/user/create', 
     zv('json' , createitfCreate) ,
     async (c) => {
@@ -38,6 +39,7 @@ page.post('/user/create',
 
 // ================================ Room =============================
 const roomitfcreate = z.object({
+    uid : z.int("missing param") ,
     Name : z.string("Need name for this room").min(4 , "need to be atleast 4 charactor"),
     House : z.string("need house name for this room")
 })
@@ -46,19 +48,18 @@ page.post('/Room/create/:uid' ,
     zv('json' , 
     roomitfcreate) , async (c) => {
     try{
-        const uid = c.req.param('uid')
-        const newroom = c.req.valid('json');
-        const hid = await callDB(c , "SELECT H_id from Home WHERE U_id = ? AND H_name = ?").bind(uid , newroom.House).run()
+        const body  = c.req.valid('json');
+        const hid = await callDB(c , "SELECT H_id from Home WHERE U_id = ? AND H_name = ?").bind(body.uid , body.House).run()
         const createRoom  = await callDB(c 
             , "INSERT INTO Room(r_name , H_id) values( ? , ?)")
         .bind(
-            newroom.Name,
+            body.Name,
             hid.results[0].H_id
         ).run();
 
         c.status(201);
         console.log(createRoom.meta)
-        return c.json({created : newroom});
+        return c.json({created : body});
     }catch(e:any){ 
         c.status(500);
         return c.json({error : e.massage}); 
@@ -70,30 +71,89 @@ page.post('/Room/create/:uid' ,
 
 
 const housecreate = z.object({
+    uid : z.int("missing param") , 
     Name : z.string("require name for this House")
     .min(4 ,"need to be atleast 4 character")
 })
-
-page.post('/Home/create/:uid' , 
+page.post('/Home/create' , 
     zv('json' , housecreate) ,
     async  (c) => {
     try {
-        const uid = c.req.param('uid');
-        const newhouse = c.req.valid('json')
-        const createhouse = await callDB(c ,
-            "INSERT INTO Home ( H_name , U_id ) VALUES (? , ? )")
-        .bind(
-            newhouse.Name,
-            uid
-        ).run()
-        c.status(201)
-        console.log(createhouse.meta)
-        return c.json({created : newhouse})
-    }catch(e:any){ 
+        const body = c.req.valid('json')
+                
+        const conf = await callDB(c , `SELECT COUNT(H_id) as count FROM Home WHERE U_id = ? and H_name = ? `)
+        .bind( body.uid , body.Name).all();
+
+        if ( conf.count > 0 ){
+                c.status(409);
+                const e = {error : "This house already exist"}  
+                console.log({error : "This house already exist" ,
+                    confic : { House_name : body.Name , User_id : body.uid}
+                })
+                return c.json(e)
+        }
+
+        else {
+            const createhouse = await callDB(c ,
+                "INSERT INTO Home ( H_name , U_id ) VALUES (? , ? )" )
+                .bind(
+                    body.Name,
+                    body.uid
+                ).run()
+                
+                c.status(201)
+                console.log(createhouse)
+                return c.json({created : body})
+            }
+
+    }catch(e:any){
+
         c.status(500);
-        return c.json({error : e.massage}); 
+        return c.json({error : e}); 
     }
 })
+// ============================================= Borad =============================
+
+const Bbodycreate  = z.object ({
+    uid : z.int("missing param user"),
+    name : z.string("Need a namefor board"),
+    rid :  z.int("missing param room") , 
+    Hid  :   z.int("Missing param house") 
+} )
+
+
+page.post('/Board/create' , 
+    zv('json' , Bbodycreate) , async (c) => {
+        try {
+            const body = c.req.valid('json')
+
+            const exist = await callDB(c , `SELECT COUNT(r.r_id) as count FROM Room r 
+                INNER JOIN Home h ON r.H_id = h.H-id
+                INNER JOIN User u  ON h.U_id = u.u_id
+                WHERE u.U_id = ? AND r.r_id = ? and H_id = ? `)
+                .bind( body.uid , body.rid , body.Hid).all()
+
+            if (exist.count <= 0 ){
+                return c.json({error : "room not found"})
+            }
+            else { 
+                const results = await callDB(c , `INSERT INTO  Board (B_name , r_id , status)
+                    VALUES (?,?,?)`).bind 
+                .bind(
+                    body.name,
+                    body.rid,
+                    0 ).run()
+
+                console.log(results.meta);
+                c.status(201);
+                return c.json({ result : "created"})
+            }
+        }catch(e:any){
+            console.log({ error : e.message})
+            return c.json ({result : "error" })
+        }
+    })
+
 
 
 export default page ; 
